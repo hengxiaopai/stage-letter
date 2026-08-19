4b2cb5155d1ef1f033687cea9dc5191fb9b04f66
JadeHeng <hengxiaopai@gmail.com>
2026-08-19 15:20:03 +0800

    feat(gate1.3): expose formal multiplatform registry surface

--- stage_letter/infrastructure/platforms/__init__.py
+++ stage_letter/infrastructure/platforms/__init__.py
@@ -2,7 +2,11 @@
 
 from .bilibili import BilibiliFormalAdapter
 from .bilibili_http import BilibiliHttpGateway
+from .douyin import DouyinFormalAdapter
 from .douyin_streamget import STREAMGET_DOUYIN_SOURCE, StreamGetDouyinGateway
+from .douyu import DouyuFormalAdapter
+from .douyu_http import DouyuHttpGateway
+from .factory import FORMAL_PLATFORMS, build_formal_adapter_registry
 from .failures import (
     ProviderFailure,
     ProviderFailureKind,
@@ -12,6 +16,8 @@ from .failures import (
     normalize_explicit_status,
     unknown_snapshot_for_failure,
 )
+from .huya import HuyaFormalAdapter
+from .huya_http import HuyaHttpGateway
 from .registry import AdapterNotFoundError, AdapterRegistry
 
 __all__ = [
@@ -19,11 +25,18 @@ __all__ = [
     "AdapterRegistry",
     "BilibiliFormalAdapter",
     "BilibiliHttpGateway",
+    "DouyinFormalAdapter",
+    "DouyuFormalAdapter",
+    "DouyuHttpGateway",
+    "FORMAL_PLATFORMS",
+    "HuyaFormalAdapter",
+    "HuyaHttpGateway",
     "ProviderFailure",
     "ProviderFailureKind",
     "ProviderOperationError",
     "STREAMGET_DOUYIN_SOURCE",
     "StreamGetDouyinGateway",
+    "build_formal_adapter_registry",
     "classify_exception",
     "classify_http_failure",
     "normalize_explicit_status",
