11ced9d9e24e8246c880764a81a16cccc80c37f5
JadeHeng <hengxiaopai@gmail.com>
2026-08-20 12:50:40 +0800

    gate2.3: export telemetry repository

--- stage_letter/infrastructure/detection/__init__.py
+++ stage_letter/infrastructure/detection/__init__.py
@@ -1,5 +1,9 @@
 """Gate 2 operational detection infrastructure."""
 
 from .scheduling import SQLAlchemyDetectionScheduleRepository
+from .telemetry import SQLAlchemyDetectionTelemetryRepository
 
-__all__ = ["SQLAlchemyDetectionScheduleRepository"]
+__all__ = [
+    "SQLAlchemyDetectionScheduleRepository",
+    "SQLAlchemyDetectionTelemetryRepository",
+]
